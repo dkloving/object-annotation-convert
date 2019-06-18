e0f79d712c26250325f0f6b3e40e7d23bf56d3d3
David <davidkloving@gmail.com>
2019-06-19 00:23:59 +0300

    cleaned up handling of invalid images to allow user to decide whether to include them or not after reading label files.
    added make_dataset method to produce a dataset from a fitted reader.

--- modules/pascalvoc.py
+++ modules/pascalvoc.py
@@ -5,13 +5,14 @@ import pandas as pd
 from pathlib import PurePath
 from warnings import warn
 
+from modules.dataset import Dataset
 from utils.io import read_image_file
 
 
 class PascalVOCReader:
     """TODO: docstring"""
 
-    def __init__(self, label_folder, image_folder_override=None, include_missing_images=False):
+    def __init__(self, label_folder, image_folder_override=None):
         """ TODO: docstring"""
         if not os.path.isdir(label_folder):
             raise ValueError('Label folder {} not a valid directory'.format(label_folder))
@@ -20,21 +21,25 @@ class PascalVOCReader:
                 raise ValueError('Label folder {} not a valid directory'.format(label_folder))
         self._image_folder_override = image_folder_override
         self._label_folder = label_folder
-        # self._invalid_image_refs = ()
         self._dataframe = pd.DataFrame()
-        self._ignore_missing_images = include_missing_images
+        self._class_ids = dict()
 
-    def fit(self, validate_images=False):
+    def make_dataset(self, include_invalid_images=False):
+        source_dataframe = self._dataframe.drop('image_valid', axis=1) if include_invalid_images else\
+            self._dataframe[self._dataframe.image_valid].drop('image_valid', axis=1)
+        dataset = Dataset(source_object=source_dataframe, image_read_method=read_image_file)
+        return dataset
+
+    def fit(self, deep_validate_images=False):
         """TODO: docstring"""
-        if validate_images:
-            warn("Validating images can be very slow on large datasets.")
-            # TODO: add actual validation of images
+        if deep_validate_images:
+            warn("Deep validation of images can be very slow on large datasets.")
         label_files = glob.glob(os.path.join(self._label_folder, '*.xml'))
         objects_df = pd.DataFrame()
         for label_file in label_files:
             with open(label_file, 'r') as file:
                 xml = etree.fromstring(file.read())
-                new_df = self.__xml_to_dataframe(xml)
+                new_df = self.__xml_to_dataframe(xml, deep_validate_images)
                 objects_df = pd.concat([objects_df, new_df])
         self._dataframe = objects_df
         return self
@@ -42,36 +47,46 @@ class PascalVOCReader:
     def read(self):
         pass
 
-    def __xml_to_dataframe(self, xml):
+    def __validate_image(self, image_path, deep, width, height, depth):
+        if deep:
+            raise NotImplementedError
+        else:
+            return os.path.isfile(image_path)
+
+    def __xml_to_dataframe(self, xml, deep_validate_images):
         """ TODO: docstring"""
         if self._image_folder_override is None:
             image_id = str(PurePath(xml.find('path').text))  # PurePath handles POSIX / Win32 differences
         else:
             image_id = str(PurePath(self._image_folder_override).joinpath(PurePath(xml.find('filename').text)))
-        if not self._ignore_missing_images:
-            if not os.path.isfile(image_id):  # check for invalid image file
-                self._invalid_image_refs = self._invalid_image_refs + (image_id,)
-                return pd.DataFrame()
         image_width = xml.find('size').find('width').text
         image_height = xml.find('size').find('height').text
         image_depth = xml.find('size').find('depth').text
+        image_valid = self.__validate_image(image_id, deep_validate_images, image_width, image_height, image_depth)
         objects = []
         for item in xml:
             if item.tag == 'object':
-                objects.append({'class_name': item.find('name').text,
+                class_name = item.find('name').text
+                if class_name not in self._class_ids:
+                    self._class_ids[class_name] = len(self._class_ids)
+                class_id = self._class_ids[class_name]
+                objects.append({'class_name': class_name,
+                                'class_id': class_id,
                                 'x_min': item.find('bndbox').find('xmin').text,
                                 'x_max': item.find('bndbox').find('xmax').text,
                                 'y_min': item.find('bndbox').find('ymin').text,
                                 'y_max': item.find('bndbox').find('ymax').text
                                 })
         objects = pd.DataFrame(objects)
-        for item in ['image_id', 'image_width', 'image_height', 'image_depth']:
+        for item in ['image_id', 'image_width', 'image_height', 'image_depth', 'image_valid']:
             objects[item] = eval(item)
-        objects['retrieval_func'] = 'pascalvoc_image_reader'
         return objects
+
+    def _get_invalid_images(self):
+        return self._dataframe[~self._dataframe.image_valid].image_id.unique()
 
 
 class PascalVOCWriter:
 
     def __init__(self):
-        raise NotImplemented
+        raise NotImplementedError
--- modules/pascalvoc_test.py
+++ modules/pascalvoc_test.py
@@ -13,12 +13,12 @@ class TestPascalVOC(unittest.TestCase):
     def test_read(self):
         # correctly read and count files while rejecting non-existing images
         reader = PascalVOCReader(label_folder=self.test_folder_labels).fit()
-        self.assertEqual(len(reader._invalid_image_refs), 4)
+        self.assertEqual(len(reader._get_invalid_images()), 4)
         # correctly read and count files with overridden image folder
-        reader = PascalVOCReader(label_folder=self.test_folder_labels, image_folder_override=self.test_folder_images).fit()
+        reader = PascalVOCReader(label_folder=self.test_folder_labels,
+                                 image_folder_override=self.test_folder_images).fit()
         self.assertEqual(describe_folders(reader._dataframe['image_id'])[-1], '4')
-        # correctly read and count files without rejection of non-existing images
-        reader = PascalVOCReader(label_folder=self.test_folder_labels, include_missing_images=True).fit()
-        self.assertEqual(len(reader._invalid_image_refs), 0)
-        self.assertEqual(describe_folders(reader._dataframe['image_id'])[-1], '4')
-
+        self.assertEqual(len(reader._get_invalid_images()), 0)
+        # test make dataset produces dataset with all entries
+        dataset = reader.make_dataset()
+        self.assertEqual(len(dataset), len(reader._dataframe))
